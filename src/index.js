8682915cc6a5b5273e574a78bfc31dee47ced7fa
Jake Ascher <ascherj@gmail.com>
2022-07-14 17:28:01 -0600

    Add comments and refactor

--- src/index.js
+++ src/index.js
@@ -5,35 +5,46 @@ const fs = require('fs');
 const { NUMBER_MAP } = require('./number_mapping.js');
 
 const getAllEpisodeURLs = async (page) => {
+  // Load page
   await page.goto('https://musicforprogramming.net/latest', { waitUntil: 'domcontentloaded' });
 
+  // Get URLs from all <a> elements
   const results = await page.evaluate(() => {
     const elems = Array.from(document.querySelectorAll('a'));
     const urls = elems.map(elem => elem.href);
     return urls;
   });
 
+  // Determine which URLs are episode URLs
   const episodeURLs = results.filter(url => NUMBER_MAP.hasOwnProperty(url.split('/').pop()));
 
   return episodeURLs;
+}
+
+const getMP3URL = async (page, episodeURL) => {
+  // Load page
+  await page.goto(episodeURL, { waitUntil: 'networkidle0' });
+  console.log('Loaded page:', episodeURL);
+
+  // Get mp3 URL
+  const mp3URL = await page.evaluate(() => {
+    const elems = Array.from(document.querySelectorAll('section#content a'));
+    return elems[0].href;
+  });
+
+  return mp3URL;
 }
 
 (async () => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    const urls = await getAllEpisodeURLs(page);
 
-    for (let i = 0; i < urls.length; i++) {
-      // Load page
-      await page.goto(urls[i], { waitUntil: 'networkidle0' });
-      console.log('Loaded page:', urls[i]);
+    const episodeURLs = await getAllEpisodeURLs(page);
 
-      // Get mp3 URL
-      const mp3URL = await page.evaluate(() => {
-        const elems = Array.from(document.querySelectorAll('section#content a'));
-        return elems[0].href;
-      });
+    for (let i = 0; i < episodeURLs.length; i++) {
+      const episodeURL = episodeURLs[i];
+      const mp3URL = await getMP3URL(page, episodeURL);
 
       // Download mp3 file
       const currentDownload = new Promise((resolve, reject) => {
@@ -48,6 +59,7 @@ const getAllEpisodeURLs = async (page) => {
           const path = `mp3s/${mp3URL.split('/').pop()}`;
           const writeStream = fs.createWriteStream(path);
 
+          // Write .mp3 to disk
           res.pipe(writeStream);
 
           writeStream.on('finish', () => {
@@ -61,7 +73,6 @@ const getAllEpisodeURLs = async (page) => {
       await currentDownload;
     }
     console.log('Downloaded all .mp3 files');
-
     await browser.close();
     console.log('Browser closed!');
   } catch (err) {
